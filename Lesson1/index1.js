a780f0ab9e4b4d4072761188a930c4a1f1852a53
vsynychych <vsynychych@gmail.com>
2020-10-29 23:03:53 +0200

    Lesson1

---
+++ Lesson1/index1.js
@@ -0,0 +1,246 @@
+//Завдання 1
+// let hi="hello";
+// let o="owu";
+// let dom="com";
+// let lang="ua";
+// let one="1";
+// let ten="10";
+// let nine="999";
+// let hundred="123";
+// let pi="3.14";
+// let e="2.7";
+// let sixteen="16";
+// let yes="true";
+// let no="false";
+// console.log(hi);
+// console.log(o);
+// console.log(dom);
+// console.log(lang);
+// console.log(one);
+// console.log(ten);
+// console.log(nine);
+// console.log(hundred);
+// console.log(pi);
+// console.log(e);
+// console.log(sixteen);
+// console.log(yes);
+// console.log(no);
+// alert(hi);
+// alert(o);
+// alert(dom);
+// alert(lang);
+// alert(one);
+// alert(ten)
+// alert(nine);
+// alert(hundred);
+// alert(pi);
+// alert(e);
+// alert(sixteen);
+// alert(yes);
+// alert(no);
+// document.write(hi);
+// document.write(o);
+// document.write(dom);
+// document.write(lang);
+// document.write(one);
+// document.write(ten);
+// document.write(nine);
+// document.write(hundred);
+// document.write(pi);
+// document.write(e);
+// document.write(sixteen);
+// document.write(yes);
+// document.write(no);
+
+
+// Завдання 2
+// let hi="hi";
+// let o="ok-ten";
+// let dom="net";
+// let lang="de";
+// let one="1.01";
+// let ten="10.10";
+// let nine="9";
+// let hundred="100";
+// let pi="3,14159265";
+// let e="2.71";
+// let sixteen="16.01";
+// let yes="no-false";
+// let no="no-true";
+// console.log(hi);
+// console.log(o);
+// console.log(dom);
+// console.log(lang);
+// console.log(one);
+// console.log(ten);
+// console.log(nine);
+// console.log(hundred);
+// console.log(pi);
+// console.log(e);
+// console.log(sixteen);
+// console.log(yes);
+// console.log(no);
+// alert(hi);
+// alert(o);
+// alert(dom);
+// alert(lang);
+// alert(one);
+// alert(ten)
+// alert(nine);
+// alert(hundred);
+// alert(pi);
+// alert(e);
+// alert(sixteen);
+// alert(yes);
+// alert(no);
+// document.write(hi);
+// document.write(o);
+// document.write(dom);
+// document.write(lang);
+// document.write(one);
+// document.write(ten);
+// document.write(nine);
+// document.write(hundred);
+// document.write(pi);
+// document.write(e);
+// document.write(sixteen);
+// document.write(yes);
+// document.write(no);
+
+
+// Завдання 3
+// const name1="Vitaliy";
+// const name2="Bogdan";
+// const name3="Taras";
+// const number1=+"1";
+// const number2=+"2";
+// const number3=+"3";
+//
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// alert(name1);
+// alert(name2);
+// alert(name3);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// document.write(name1);
+// document.write(name2);
+// document.write(name3);
+// document.write(number1);
+// document.write(number2);
+// document.write(number3);
+
+
+// Завдання 4-5
+// let name=prompt("Імя");
+// let surname=prompt("Прізвище")
+// let patronymic=prompt("По батькові")
+// console.log(name);
+// console.log(surname);
+// console.log(patronymic);
+// alert(name);
+// alert(surname);
+// alert(patronymic);
+// document.write(name);
+// document.write(surname);
+// document.write(patronymic);
+//
+// let person="name, surname, patronymic";
+// console.log(person);
+// alert(person);
+// document.write(person);
+
+// Завдання 7
+// let number1= prompt("Введіть перше число:");
+// let number2= prompt("Введіть друге число:");
+// let number3= prompt("Введіть третє число:");
+// console.log(+number1);
+// console.log(+number2);
+// console.log(+number3);
+
+// Завдання 8
+// let number1= prompt("Введіть перше число:");
+// let number2= prompt("Введіть друге число:");
+// let number3= prompt("Введіть третє число:");
+// let number4= prompt("Введіть четверте число:");
+// let result=number1+number2+number3+number4;
+// console.log(parseInt(number1));
+// console.log(parseInt(number2));
+// console.log(parseInt(number3));
+// console.log(parseInt(number4));
+// console.log(parseInt(result));
+
+// Завдання 9
+// let number1= prompt("Введіть перше число:");
+// let number2= prompt("Введіть друге число:");
+// let number3= prompt("Введіть третє число:");
+// console.log(parseFloat(number1));
+// console.log(parseFloat(number2));
+// console.log(parseFloat(number3));
+// let result=number1+number2+number3;
+// console.log(result);
+
+
+// Завдання 10
+// let number1= +prompt("Введіть перше число:");
+// let number2= +prompt("Введіть друге число:");
+// let number3= +prompt("Введіть третє число:");
+// console.log(Math.round(number1))
+// console.log(Math.round(number2))
+// console.log(Math.round(number3))
+// console.log(result);
+
+// Завдання 11
+// let number1= +prompt("Введіть перше число:");
+// let number2= +prompt("Введіть друге число:");
+// let first=Math.round(number1);
+// let second=Math.round(number2)
+// console.log(Math.pow(first,second));
+
+// Завдання 12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// Завдання 13
+// let a=5<6;
+// let b=5>6;
+// let c=5>=6;
+// let d=5===6;
+// let e=10===10;
+// let f=10>=10;
+// let g=10<10;
+// let h=10>10;
+// let i=10!==10;
+// let j=123==='123';
+// let k=123 !=='123';
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+// console.log(k);
+
+
+
+
+
+
+
+
+
---
+++ Lesson1_class/class1.js
@@ -0,0 +1,120 @@
+
+// Завдання 1
+// let str="Привет";
+// let num=123;
+// let flag=true;
+// let txt="true";
+
+// Завдання 2
+// let a1=5+3;
+// let a2=5-3;
+// let a3=5*3;
+// let a4=5/3;
+// let a5=5%3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+//
+// Завдання 3
+// let a6=5%3;
+// let a7=3%5;
+// let a8=5+"3";
+// let a9="5"-3;
+// let a10=75+"кг";
+// console.log(a6);
+// console.log(a7);
+// console.log(+a8);
+// console.log(+a9);
+// console.log(a10);
+
+// Завдання 4
+// let height=23;
+// let width=10;
+// let s= height*width;
+// console.log(s);
+//
+// Завдання 5
+// let height=10;
+// let dC=4;
+// let radius=dC/2;
+// const pi=3.14;
+// let v=pi*(radius*radius)*height
+// console.log(v);
+//
+//Завдання 6
+// let n=3;
+// let m=4;
+// let k=Math.sqrt(Math.pow(n,2)+Math.pow(m,2));
+// console.log(k);
+
+//Завдання 7
+// let str="Hello world";
+// document.write(str);
+// alert(str);
+// console.log(str);
+
+
+// Завдання 8
+// let name="Синичич \nВіталій \nВолодимирович";
+// let age=30;
+// let hobby="workout \n learning programming";
+// alert(name);
+// alert(age);
+// alert(hobby);
+
+// Завдання 9
+// let str1="Хто "
+// let str2="Ти "
+// let str3="Такий?"
+// let concatenation= str1 + str2 + str3;
+// document.write(concatenation);
+
+// Завдання 10
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+// Завдання 11
+// let pi= parseInt("3.14")
+// let minus=parseInt("-7.875")
+// let plus= parseInt("435")
+// let name=parseInt("Вася")
+// console.log(pi);
+// console.log(minus);
+// console.log(plus);
+// console.log(name);
+
+
+//Завдання 12
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+// Завдання 13
+// let first=prompt("Введіть перше число:");
+// let second=prompt("Введіть друге число:");
+// let result=parseInt(first)+parseInt(second);
+// alert(result);
+
+// Завдання 14
+let name=prompt("Введіть Імя:");
+let surname=prompt("_Введіть прізвище:");
+let age=prompt("Введіть ваш вік:");
+let result="  Доброго вечора "+name+ surname + ", мої вітання що вам " + parseInt(age);
+alert(result);
+
+
+
+// ДОДАТКОВІ
+// Завдання 1
+// let first=prompt("Введіть перше число:");
+// let second=prompt("Введіть друге число:");
+// let third=prompt("Введіть третє число:");
+//
+// if (first>second&&first>third){
+//     alert(first);
+// }
