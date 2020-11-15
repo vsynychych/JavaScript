fb93b127b11eb5da215bdb2b4f1a93045dd58ada
vsynychych <vsynychych@gmail.com>
2020-11-15 23:56:47 +0200

    Lesson4

--- Lesson4/Lesson4_1.js
+++ Lesson4/Lesson4_1.js
@@ -293,20 +293,152 @@
 
 // - приймає масив автомобілів (можна взяти з попередніх дз ),
 // та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-// let arr=[{model:"passat", year:2000, ps:101, color:"black"},
-//     {model:"polo", year:2010, ps:90, color:"white"},
-//     {model:"amarok", year:2001, ps:118, color:"red"},
-//     {model:"jetta", year:2012, ps:200, color:"blue"},
-//     {model:"caddy", year:2003, ps:131, color:"green"},
-//     {model:"golf", year:2014, ps:250, color:"silver"},
-//     {model:"eos", year:2005, ps:101, color:"black"},
-//     {model:"scirocco", year:2016, ps:170, color:"yellow"},
-//     {model:"touran", year:2007, ps:118, color:"silver"},
-//     {model:"touareg", year:2018, ps:150, color:"black"},
+// let arrCars=[{id:1,model:"passat", year:2000, ps:101, color:"black"},
+//     {id:2,model:"polo", year:2010, ps:90, color:"white"},
+//     {id:3,model:"amarok", year:2001, ps:118, color:"red"},
+//     {id:4,model:"caddy", year:2003, ps:131, color:"green"},
+//     {id:5,model:"golf", year:2014, ps:250, color:"silver"},
+//     {id:6,model:"eos", year:2005, ps:101, color:"black"},
+//     {id:7,model:"jetta", year:2012, ps:200, color:"blue"},
+//     {id:8,model:"scirocco", year:2016, ps:170, color:"yellow"},
+//     {id:9,model:"touran", year:2007, ps:118, color:"silver"},
+//     {id:10,model:"touareg", year:2018, ps:150, color:"black"},
 // ]
+// function cars(arr,Tag){
+//     let idList=document.getElementById(Tag);
+//     let ul=document.createElement("ul");
+//     for (let i = 0; i < arr.length; i++) {
+//         let car=arr[i];
+//         let li=document.createElement("li");
+//         li.innerText=`${i+1} - ${car.model}`;
+//         ul.appendChild(li);
+//     }
+//     idList.appendChild(ul);
+// }
+// cars(arrCars,"list");
+
+//     Для кожного автомобіля створити свій блок, та додати його в елемент,
+//     індентифікатор якого ви отримали. Всі властивості авто в обному блоці
+// function cars(arr,Tag){
+//     let idList=document.getElementById(Tag);
+//     let div=document.createElement("div");
+//     for (let i = 0; i < arr.length; i++) {
+//         let car=arr[i];
+//         let h1=document.createElement("h1");
+//         let h2=document.createElement("h2");
+//         h1.innerText=`${i+1} : ${car.model}`;
+//         h2.innerText=`Year-${car.year}; PS-${car.ps}; Color-${car.color}`;
+//         div.appendChild(h1);
+//         div.appendChild(h2);
+//     }
+//     idList.appendChild(div);
+// }
+// cars(arrCars,"list");
 
 
-//     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
-// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-//     Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
+
 //     Для кожної властивості створити всередені блока автомоблія свій блок
+// function cars(arr,Tag){
+//     let idList=document.getElementById(Tag);
+//     let first=document.createElement("div");
+//     for (let i = 0; i < arr.length; i++) {
+//         let car=arr[i];
+//         let carName=document.createElement("div");
+//         let year=document.createElement("p");
+//         let ps=document.createElement("p");
+//         let color=document.createElement("p");
+//         carName.innerText=`${i+1} : ${car.model}`;
+//         year.innerText=`Year-${car.year}`;
+//         ps.innerText=`PS-${car.ps}`;
+//         color.innerText=`Color-${car.color}`;
+//
+//         first.appendChild(carName);
+//         carName.appendChild(year);
+//         carName.appendChild(ps);
+//         carName.appendChild(color);
+//     }
+//     idList.appendChild(first);
+// }
+// cars(arrCars,"list");
+
+
+
+// (на основі минулого ДЗ)
+// **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача
+// та місто з відповідними "id" та "user_id",
+// та повертає масив цих з'єднаних об'єктів.
+// Приклад масивів:
+// let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false},
+//                    {id: 2, name: 'petya', age: 30, status: true},
+//                    {id: 3, name: 'kolya', age: 29, status: true},
+//                    {id: 4, name: 'olya', age: 28, status: false},];
+// let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'},
+//                     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//                     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//                     {user_id: 4, country: 'USA', city: 'Miami'},];
+//     function array(arrUsers,arrCities){
+//         let users=JSON.stringify(usersWithId);
+//         let users1=JSON.parse(users);
+//         let cities=JSON.stringify(citiesWithId);
+//         let cities1=JSON.parse(cities);
+//         for (let user of users1) {
+//             for (let city of cities1) {
+//                 if (city.user_id===user.id){
+//                     user.address=city;
+//                 }
+//             }
+//         }
+//         return users1
+//     }
+// console.log(usersWithId);
+// console.log(citiesWithId);
+// console.log(("________________"));
+// let array1=array(usersWithId,citiesWithId);
+// console.log(array1);
+
+
+
+
+// ***- беремо завдання з правилами з укроку №3 :
+// Та робимо це функцією.При цьому правила отримувати через аргумент.
+// "Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+// При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+//     Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+// Контентом сторінки є заголовки та параграфи.
+// Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
+// створити скрипт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li),
+// який буде змістом того, що знаходиться на сторінці.
+let rules = [
+    {
+        title: 'Первое правило Бойцовского клуба.',
+        body: 'Никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Второе правило Бойцовского клуба.',
+        body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+    },
+    {
+        title: 'Третье правило Бойцовского клуба.',
+        body: 'В схватке участвуют только двое.'
+    },
+    {
+        title: 'Четвертое правило Бойцовского клуба.',
+        body: 'Не более одного поединка за один раз.'
+    },
+    {
+        title: 'Пятое правило Бойцовского клуба.',
+        body: 'Бойцы сражаются без обуви и голые по пояс.'
+    },
+    {
+        title: 'Шестое правило Бойцовского клуба.',
+        body: 'Поединок продолжается столько, сколько потребуется.'
+    },
+    {
+        title: 'Седьмое правило Бойцовского клуба.',
+        body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+    },
+    {
+        title: 'Восьмое и последнее правило Бойцовского клуба.',
+        body: 'Новичок обязан принять бой.'
+    },
+];
