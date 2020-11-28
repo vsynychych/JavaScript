c29b8cdd193cc5218ca467ba0c5a22f0c02b331c
vsynychych <vsynychych@gmail.com>
2020-11-28 15:24:00 +0200

    Lesson7

--- Lesson7/Lesson7.js
+++ Lesson7/Lesson7.js
@@ -1,5 +1,7 @@
+
 // - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так,
 // чтобы при клике на кнопку исчезал элемент с id="text".
+//
 // let text=document.createElement("div");
 // text.id="text";
 // text.innerText="ok10";
@@ -26,6 +28,7 @@
 
 
 // - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+//
 // let div=document.createElement("div");
 // let button=document.createElement("button");
 // document.body.appendChild(div);
@@ -62,6 +65,7 @@
 
 
 // - Создайте меню, которое раскрывается/сворачивается при клике
+//
 // let list=document.getElementById("list");
 // let menu=document.getElementById("menu");
 // let xxx=false;
@@ -81,6 +85,7 @@
 // - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
 //     Вывести список комментариев в документ, каждый в своем блоке.
 //     Добавьте каждому комментарию по кнопке для сворачивания его body.//
+//
 // let comments=[
 //     {title : 'lorem1', body:'1 lorem '},
 //     {title : 'lorem2', body:'2 lorem ipsum '},
@@ -125,6 +130,7 @@
 // виводиться на консоль інформація з цих 2х форм.
 // Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
 // Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
+//
 // let form1=document.getElementById("form1");
 // let form2=document.getElementById("form2");
 // let input1=document.getElementById("input1");
@@ -147,8 +153,7 @@
 //     Перший аргумент визначає кількість строк.
 //     Другий параметр визначає кліькіть ячеєк в кожній строці.
 //     Третій параметр визначає елемент в який потрібно таблицю додати.
-
-
+//
 // function table(tr, td, tag) {
 //     let table = document.createElement("table");
 //     for (let i = 0; i < tr; i++) {
@@ -174,16 +179,103 @@
 
 // - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //  При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
-//  (Додатковачастина для завдання)
-
-
-
+//  (Додаткова частина для завдання)
+//
+// let form = document.createElement("form");
+// let tr = document.createElement("input");
+// let td = document.createElement("input");
+// let text = document.createElement("input");
+// let button = document.createElement("button");
+// form.id = "MyForm";
+// tr.id = "input1";
+// td.id = "input2";
+// text.id = "input3";
+// form.appendChild(tr);
+// form.appendChild(td);
+// form.appendChild(text);
+// form.appendChild(button);
+// document.body.appendChild(form);
+// button.innerText = "OK";
+//
+// button.onclick = () => {
+//     let tr1 = document.forms.MyForm.input1.value;
+//     let td1 = document.forms.MyForm.input2.value;
+//     let text = document.forms.MyForm.input3.value;
+//     let table = document.createElement("table");
+//     let div2 = document.createElement("div")
+//     for (let i = 0; i < tr1; i++) {
+//         let tr = document.createElement("tr")
+//         for (let j = 0; j < td1; j++) {
+//             let td = document.createElement("td");
+//             tr.appendChild(td);
+//             td.innerText = text;
+//         }
+//         table.appendChild(tr);
+//     }
+//     div2.appendChild(table);
+//     document.body.appendChild(div2);
+// }
 
 
 
 // - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
-
-
+//
+// let arrImg = [
+//     {id: 1, url: "Lviv/Lviv_1.jpg"},
+//     {id: 2, url: "Lviv/Lviv_2.jpg"},
+//     {id: 3, url: "Lviv/Lviv_3.jpg"},
+//     {id: 4, url: "Lviv/Lviv_4.jpg"},
+//     {id: 5, url: "Lviv/Lviv_5.jpg"},
+// ];
+// let arrows = [
+//     {id1: 1, url: "Lviv/Left.png"},
+//     {id2: 2, url: "Lviv/Right.png"}
+// ]
+// let content = document.getElementById("content");
+// let img = document.createElement("img");
+// let left = document.createElement("img");
+// let right = document.createElement("img");
+// let aLeft = document.createElement("a");
+// let aRight = document.createElement("a");
+// aLeft.href = "#";
+// aRight.href = "#";
+//
+// let index = 0;
+//
+// left.width = 100;
+// left.src = arrows[0].url;
+//
+// img.width = 800;
+// img.height = 540;
+// img.src = arrImg[index].url;
+//
+// right.width = 100;
+// right.src = arrows[1].url;
+//
+// content.appendChild(aLeft);
+// aLeft.appendChild(left);
+// content.appendChild(img);
+// content.appendChild(aRight);
+// aRight.appendChild(right);
+//
+// left.onclick = () => {
+//     if (index - 1 < 0) {
+//         index = arrImg.length - 1
+//     } else {
+//         index = index - 1
+//     }
+//     img.src = arrImg[index].url;
+// }
+//
+// right.onclick = () => {
+//     if (index + 1 > arrImg.length - 1) {
+//         index = 0;
+//     } else {
+//         index = index + 1
+//     }
+//
+//     img.src = arrImg[index].url;
+// }
 
 
 
@@ -193,7 +285,30 @@
 // Якщо людина вводить слово і воно міститься в масиві не цензурних слів
 // кинути алерт з попередженням.
 // Перевірку робити при натисканні на кнопку
-
+//
+// let arr=["shit","bullshit","bastard","fuck","stupid","freak"];
+//
+// let form=document.createElement("form");
+// form.id="MyForm";
+// let input= document.createElement("input");
+// input.id="MyInput"
+// input.type="text";
+// let button=document.createElement("button");
+// button.innerText="OK";
+//
+//
+// form.appendChild(input);
+// form.appendChild(button);
+// document.body.appendChild(form);
+//
+// button.onclick=()=>{
+//     arr.forEach(el=>{
+//         let x=document.forms.MyForm.MyInput.value;
+//         if (x===el){
+//             alert("WARNING!!!");
+//         } stop();
+//     })
+// }
 
 
 
@@ -204,19 +319,67 @@
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+//
+// let arr = ["shit", "bullshit", "bastard", "fuck", "stupid", "freak"];
+//
+// let form = document.createElement("form");
+// form.id = "MyForm";
+// let input = document.createElement("input");
+// input.id = "MyInput"
+// input.type = "text";
+// let button = document.createElement("button");
+// button.innerText = "OK";
+//
+// form.appendChild(input);
+// form.appendChild(button);
+// document.body.appendChild(form);
+//
+// button.onclick = () => {
+//     let x = document.forms.MyForm.MyInput.value;
+//     arr.forEach(el => {
+//         let xxx = x.split(" ");
+//         xxx.forEach(ell => {
+//             if (ell === el) {
+//                 alert("WARNING")
+//             }
+//             stop();
+//         })
+//     })
+// }
 
 
 
 
 
-// - создать скрипт, который берет считывает на странице (rules.html) текст и делает сбоку меню-оглавление по всем заголовкам которые есть в тексте.
-//     При клике на пункт оглавления вы должны отправляться к этому пункту в тексте
-
+// - создать скрипт, который берет считывает на странице (rules.html) текст и делает сбоку меню-оглавление по всем
+// заголовкам которые есть в тексте.
+// При клике на пункт оглавления вы должны отправляться к этому пункту в тексте
+//
+// let h2=document.getElementsByTagName("h2");
+// let content=document.getElementById("content");
+// let wrap=document.getElementById("wrap");
+// let ul=document.createElement("ul");
+// for (let i = 0; i < h2.length; i++) {
+//     let li=document.createElement("li");
+//     let a=document.createElement("a");
+//     let anchor="anchor"+i;
+//     a.href="#"+anchor;
+//     h2[i].setAttribute("id",anchor);
+//     a.innerText=h2[i].innerText;
+//     li.appendChild(a);
+//     ul.appendChild(li);
+// }
+// content.appendChild(ul);
+// content.style.width="30%";
+// wrap.style.width="70%"
+// content.style.float="left";
+// wrap.style.float="right";
 
 
 
 
 // - взять массив пользователей
+//
 // let usersWithAddress = [
 //     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
 //     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
@@ -235,25 +398,63 @@
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
-
-
-
-
-
-
-
-
-// *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .
-// Функция создает в боди 2 кнопки (назад/вперед)
-// при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к
-// следующему дочернему элементу (лежащему на одном уровне)
-// НО если у (какого-либо)дочеренего элемента есть дети, то нажатие "вперед" позволяет нам войти внутрь элемента и
-// выводит первого ребенка. и тд.
-// Когда все дети заканчиваются, мы выходим из данного дочернего элемента и переходим к следующему, лежащему с ним на одном уровне
-
-
-
-
-
-
-// *** При виділені сегменту тексту на сторінці він стає жирний/курсивний/або якось іншим способом змінює свій стан
+//
+// function newUsers(arr) {
+//     let string = document.createElement("div")
+//     arr.forEach(el => {
+//         let div = document.createElement("div");
+//         div.innerText = JSON.stringify(el);
+//         string.appendChild(div);
+//     })
+//     return string;
+// }
+//
+// let x = newUsers(usersWithAddress);
+// // console.log(x);
+//
+// let content = document.getElementById("content");
+// let users = document.createElement("div");
+// users.appendChild(x);
+// content.appendChild(users);
+//
+// let input1 = document.createElement("input");
+// let input2 = document.createElement("input");
+// let input3 = document.createElement("input");
+//
+// input1.type = "checkbox";
+// input2.type = "checkbox";
+// input3.type = "checkbox";
+//
+// let label1 = document.createElement("label");
+// let label2 = document.createElement("label");
+// let label3 = document.createElement("label");
+//
+// label1.innerText = "Users status false";
+// label2.innerText = "Users старше 29 лет";
+// label3.innerText = "Users с города киев";
+//
+// let button = document.createElement("button");
+// button.innerText = "OK";
+//
+// content.appendChild(input1);
+// content.appendChild(label1);
+// content.appendChild(input2);
+// content.appendChild(label2);
+// content.appendChild(input3);
+// content.appendChild(label3);
+// content.appendChild(button);
+//
+// button.onclick = () => {
+//     let newArr = JSON.parse(JSON.stringify(usersWithAddress));
+//     if (input1.checked)
+//         newArr = newArr.filter(value => value.status === false);
+//
+//     if (input2.checked)
+//         newArr = newArr.filter(value => value.age >= 29);
+//
+//     if (input3.checked)
+//         newArr = newArr.filter(value => value.address.city === 'Kyiv');
+//
+//     users.innerText="";
+//     users.appendChild(newUsers(newArr));
+// }
