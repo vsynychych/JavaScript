38dc449f4f1864668efa5d6bec37109819aaf700
vsynychych <vsynychych@gmail.com>
2020-12-07 01:21:00 +0200

    Lesson9

--- Lesson9_Callback_Promise/Lesson9.js
+++ Lesson9_Callback_Promise/Lesson9.js
@@ -54,7 +54,7 @@ routineDay("workingDay", (err, data) => {
                                                                 console.log("((((...", err);
                                                             } else {
                                                                 console.log("Вивчив нові скіли...", data)
-                                                                sleep("no", (err, data) => {
+                                                                sleep("yes", (err, data) => {
                                                                     if (err) {
                                                                         console.log("погано спав...", err);
                                                                     } else {
@@ -144,7 +144,7 @@ function drive2(time, cb) {
 function food2(dinner, cb) {
 
     setTimeout(() => {
-        console.log("8. Вечеря:")
+        console.log("7. Вечеря:")
         if (dinner === "no") {
             cb("Без вечері...((((", null)
         } else if (dinner === "something") {
@@ -157,7 +157,7 @@ function food2(dinner, cb) {
 function IT(lesson, cb) {
 
     setTimeout(() => {
-        console.log("9. Написання коду:")
+        console.log("8. Написання коду:")
         if (lesson === "no") {
             cb("Не написав код...((((", null)
         } else if (lesson === "yes") {
@@ -171,7 +171,7 @@ function IT(lesson, cb) {
 function sleep(lesson, cb) {
 
     setTimeout(() => {
-        console.log("10. Сон:")
+        console.log("9. Сон:")
         if (lesson === "no") {
             cb("не виспався...((((", null)
         } else if (lesson === "yes") {
@@ -179,3 +179,184 @@ function sleep(lesson, cb) {
         }
     }, Math.round(Math.random() * (3000 - 900) + 900))
 }
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+// -------------Promise-----------------
+
+
+// function routineDay(day) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (day === "dayOff") {
+//                 reject("2. Можна довше поспати, проснувся о 9ій годині");
+//                 console.log("3. Весь день відпочиваю......і вчуся писати код")
+//             } else if (day === "workingDay") {
+//                 resolve( "тому проснувся о 7ій годині");
+//             }
+//         }, Math.round(Math.random() * (1000 - 500) + 500))
+//     })
+// }
+//
+//
+// function bathrooms(water) {
+// return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("2. Душ:")
+//         if (water === "no") {
+//             reject("їду на роботу не митим... день не вдався")
+//         } else if (water === "yes") {
+//             resolve( "їду на роботу чистим")
+//         }
+//     }, Math.round(Math.random() * (1500 - 700) + 700))
+// })
+// }
+//
+//
+// function food(breakfast) {
+// return new  Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("3. Сніданок:")
+//         if (breakfast === "no") {
+//             reject("їду на роботу голодним...день не вдався")
+//         } else if (breakfast === "something") {
+//             resolve( "їду на роботу ситим")
+//         }
+//     }, Math.round(Math.random() * (2000 - 800) + 800))
+// })
+// }
+//
+//
+// function drive(mask) {
+//     return new Promise((resolve, reject) => {
+//         console.log("4. Дорога на роботу:")
+//         setTimeout(() => {
+//             if (mask === "no") {
+//                 reject(" -Я забув маску...і запізнився на роботу...день не вдався((");
+//             } else if (mask === "yes") {
+//                 resolve( "Тому шо не забув маску");
+//             }
+//         }, Math.round(Math.random() * (2200 - 900) + 900))
+//     })
+// }
+//
+//
+// function work(status) {
+//     return new Promise((resolve, reject) => {
+//         console.log("5. На роботі:")
+//         setTimeout(() => {
+//             if (status === false) {
+//                 reject(" не все заплановане виконав...день не вдався((");
+//             } else if (status === true) {
+//                 resolve( "Добре відпрацював");
+//             }
+//         }, Math.round(Math.random() * (2500 - 1000) + 1000))
+//     })
+// }
+//
+//
+// function drive2(time) {
+//     return new Promise((resolve, reject) => {
+//         console.log("6. Дорога додому")
+//         setTimeout(() => {
+//             if (time > 50) {
+//                 reject("довго добираюся додому... ((");
+//             } else if (time < 50) {
+//                 resolve("Швидко доїхав додому");
+//             }
+//         }, Math.round(Math.random() * (2700 - 1100) + 1100))
+//     })
+// }
+//
+//
+// function food2(dinner) {
+// return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("7. Вечеря:")
+//         if (dinner === "no") {
+//             reject("Без вечері...((((")
+//         } else if (dinner === "something") {
+//             resolve( "Мммм")
+//         }
+//     }, Math.round(Math.random() * (2800 - 900) + 900))
+// })
+// }
+//
+//
+// function IT(lesson) {
+// return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("8. Написання коду:")
+//         if (lesson === "no") {
+//             reject("Не написав код...((((")
+//         } else if (lesson === "yes") {
+//             resolve("супер ")
+//         }
+//     }, Math.round(Math.random() * (2900 - 900) + 900))
+// })
+// }
+//
+//
+// function sleep(lesson) {
+// return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("9. Сон:")
+//         if (lesson === "no") {
+//             reject("не виспався...((((")
+//         } else if (lesson === "yes") {
+//             resolve("можна розпочинати новий день і підкорювати нові  вершини... ")
+//         }
+//     }, Math.round(Math.random() * (3000 - 900) + 900))
+// })
+// }
+//
+//
+// routineDay("workingDay").then(value => {
+//         console.log("Сьогодні робочий день.")
+//         console.log("1. Потрібно йти на роботу, "+ value);
+//         return bathrooms("yes");
+//     }
+// )
+//     .then(value => {
+//         console.log("Є вода..."+value)
+//         return food("something");
+//     })
+//     .then(value => {
+//         console.log("Сніданок приготований..."+value)
+//         return drive("yes");
+//     })
+//     .then(value => {
+//         console.log("- Приїжджаю на роботу без пригод... "+value);
+//         return work(true);
+//     })
+//     .then(value => {
+//         console.log("- Все ок... "+value);
+//         return drive2(30);
+//     })
+//     .then(value => {
+//         console.log("- Все ок... "+value);
+//         return food2("something");
+//     })
+//     .then(value => {
+//         console.log("Вечеря приготована..."+value);
+//         return IT("yes");
+//     })
+//     .then(value => {
+//         console.log("Вивчив нові скіли..."+value);
+//         return sleep("yes");
+//     })
+//     .then(value => {
+//         console.log("Добре спалося..."+value);
+//     })
+//
+//     .catch(reason => {
+//         console.log(reason);
+//     })
+//
+//     .finally(()=>{
+//         console.log("Дякую за увагу!!!!!!")
+//     })
